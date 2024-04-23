b0d2e89871f57ce83bca42956a82162a4fa41a13
Lamenkazu <333.erick@gmail.com>
2024-04-23 07:44:04 -0300

    ✨ Auth Screens

---
+++ app/src/main/java/com/example/seutreino/view/auth/AuthActivity.kt
@@ -0,0 +1,42 @@
+package com.example.seutreino.view.auth
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.seutreino.R
+import com.example.seutreino.databinding.ActivityAuthBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_auth)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        appBarConfiguration.apply {
+            title = ""
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_auth)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
--- app/src/main/java/com/example/seutreino/view/workout/WorkoutActivity.kt
+++ app/src/main/java/com/example/seutreino/view/workout/WorkoutActivity.kt
@@ -1,15 +1,7 @@
 package com.example.seutreino.view.workout
 
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.seutreino.R
 import com.example.seutreino.databinding.ActivityWorkoutBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,27 +21,6 @@ class WorkoutActivity : AppCompatActivity() {
         setSupportActionBar(binding.workoutToolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
-
-
-        try {
-            val navController: NavController = findNavController(R.id.nav_host_fragment_content_workout);
-
-            val local = intent.extras?.getString("local")
-            if(!local.equals("add")){
-                val navInflater = navController.navInflater
-                val graph = navInflater.inflate(R.navigation.nav_graph4)
-                navController.graph = graph
-            }
-
-        } catch (e: Exception) {
-            // Handle error: NavController not found
-            e.printStackTrace();
-            // OR finish(); // Considerando fechar a atividade como alternativa
-        }
-
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-
 
     }
 
